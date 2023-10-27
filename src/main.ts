590c431fc7042f33edfcc6c36a62e4c094244049
Zetab_S <songkm2@naver.com>
2023-10-27 21:25:28 +0900

    Release 1.0.0

---
+++ src/main.ts
@@ -0,0 +1,77 @@
+import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { locales } from './locale';
+
+interface localeSettings {
+	locale: string;
+}
+
+const DEFAULT_SETTINGS: localeSettings = {
+	locale: 'en'
+};
+
+export default class SetMomentLocalePlugin extends Plugin {
+	settings: localeSettings;
+
+	async onload() {
+		await this.loadSettings();
+		this.addSettingTab(new SettingTab(this.app, this));
+
+		for (const locale of window.moment.locales()) {
+			if (locale !== this.settings.locale) {
+				window.moment.updateLocale(locale, null);
+			}
+		}
+		window.moment.updateLocale(this.settings.locale, {});
+	}
+
+	onunload() {}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+function createFragmentWithHTML(html: string) {
+	return createFragment((documentFragment) => (documentFragment.createDiv().innerHTML = html));
+}
+
+class SettingTab extends PluginSettingTab {
+	plugin: SetMomentLocalePlugin;
+
+	constructor(app: App, plugin: SetMomentLocalePlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl).setName(
+			createFragmentWithHTML(
+				`<p style="color: red">WARNING: DO NOT DELETE or DISABLE this plugin.</p>
+        Obsidian basically does NOT support to change internal "moment.js" locale.<br>
+        As a result, I couldn't fix the bug where the locale does not change when the plugin is disabled.`
+			)
+		);
+
+		new Setting(containerEl)
+			.setName('Locale')
+			.setDesc('* Restart needed')
+			.addDropdown((dropdown) => {
+				for (const code in locales) {
+					dropdown.addOption(code, locales[code]);
+				}
+
+				dropdown.setValue(this.plugin.settings.locale).onChange(async (value) => {
+					this.plugin.settings.locale = value;
+					await this.plugin.saveSettings();
+				});
+			});
+	}
+}
